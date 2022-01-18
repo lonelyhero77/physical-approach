9ce0b2d1de67c5ba2c89a79fddd94327d06cb96a
Taesoon Kim <lonelyhero77@gmail.com>
2022-01-18 11:01:03 +0900

    Clean up code

--- runge-kutta/simple-pendulum.py
+++ runge-kutta/simple-pendulum.py
@@ -6,10 +6,10 @@ import sys
 l = 1. #m
 g = 9.81 # m/s^2
 angvel0 = 0
-ang0 = 10.
+ang0 = 120.
 
 # Time Settings
-tmax = 10.
+tmax = 1000.
 tstep = 0.01
 t = np.linspace(0., tmax, int(tmax/tstep))
 
@@ -25,25 +25,25 @@ def drawProgressBar(percent, barLen=20):
     sys.stdout.flush()
 
 def derivate(X, t):
-    # Remember np.array needs Angle in radians!!!!
+    # Remember np.sin needs Angle in radians!!!!
     return np.array([X[1], -g/l * np.sin(X[0] * np.pi / 180.)])
 
 def Euler(func, X0, t):
-    dt = t[1] - t[0]
+    # dt = t[1] - t[0]
     nt = len(t)
     X = np.zeros([nt, len(X0)])
     X[0] = X0
     print("Euler method calculation")
     for i in range(nt-1):
         X[i+1] = X[i] + func(X[i], t[i]) * dt
-        if i % 10000:
+        if i % 20000:
             percent = (i / (tmax/tstep))
             drawProgressBar(percent)
     print(" Done!")
     return X
 
 def RK4(func, X0, t):
-    dt = t[1] - t[0]
+    # dt = t[1] - t[0]
     nt = len(t)
     X = np.zeros([nt, len(X0)])
     X[0] = X0
@@ -54,7 +54,7 @@ def RK4(func, X0, t):
         k3 = func(X[i] + dt/2 * k2, t[i] + dt/2.)
         k4 = func(X[i] + dt * k3, t[i] + dt)
         X[i+1] = X[i] + dt / 6. * (k1 + 2. * k2 + 2. * k3 + k4)
-        if i % 10000:
+        if i % 20000:
             percent = (i / (tmax/tstep))
             drawProgressBar(percent)
     print(" Done!")
